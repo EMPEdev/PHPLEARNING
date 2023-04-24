bde3e51195371995ab371916141a42d30c8a7aec
Patryk <maleszewskipatryk3@gmail.com>
2023-04-24 22:23:09 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>php</title>
+</head>
+<body>
+
+<?php
+echo "PHP";
+
+$name = "Patryk";
+$age = "20";
+
+$agereq = 18;
+
+if($agereq >= 18){
+	echo "Jesteś pełnoletni";
+}
+	else {
+		echo "Nie jesteś pełnoletni";
+	}
+
+?>
+
+</body>
+</html>
